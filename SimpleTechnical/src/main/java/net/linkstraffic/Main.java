85a58eaf7aea04193de48321a468e85be395c36b
Your Name <you@example.com>
2020-11-17 18:48:58 +0100

    Project initialization

---
+++ SimpleTechnical/src/main/java/net/linkstraffic/Main.java
@@ -0,0 +1,18 @@
+package net.linkstraffic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        logger.trace("trace log");
+        logger.debug("debug log");
+        logger.info("info log");
+        logger.warn("warn log");
+        logger.error("error log");
+    }
+}
